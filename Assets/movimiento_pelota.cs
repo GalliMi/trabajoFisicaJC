60222a8243d7b5834f4979d00dae443b40cb04a4
gallimi <gallimilagros@gmail.com>
2025-10-30 16:55:06 -0300

    primer comit

---
+++ Assets/movimiento_pelota.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimiento_pelota : MonoBehaviour
+{
+    public float fuerza = 10f;          
+    private Rigidbody rb;
+    bool inicio = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            inicio = true;
+
+        }
+
+        if (inicio == true)
+        {
+            // torque agrega Rotacion 
+            rb.AddTorque(transform.right * fuerza, ForceMode.Force);
+        }
+    }
+}
